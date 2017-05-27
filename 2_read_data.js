cd845fa4e45d6c695be57dfb10bbbbc1211e24df
Anatoli Makarevich <makaroni4@gmail.com>
2017-05-27 13:18:06 +0200

    Reading heart rate

--- 2_read_data.js
+++ 2_read_data.js
@@ -2,6 +2,9 @@ var noble = require("noble");
 
 var TICKR_UUID = process.env.TICKR_UUID;
 var allowDuplicates = false;
+// https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.heart_rate_measurement.xml
+var HR_CHARACTERISTIC_HEARTRATE_UUID = "2a37";
+// https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.heart_rate.xml&u=org.bluetooth.service.heart_rate.xml
 var HEART_RATE_SERVICE_UUID = "180d";
 
 noble.on("stateChange", function(state) {
@@ -27,11 +30,29 @@ noble.on("discover", function (peripheral) {
         });
 
         if(service) {
-          console.log(service);
+          service.discoverCharacteristics([
+            HR_CHARACTERISTIC_HEARTRATE_UUID
+          ], function(error, characteristics) {
+            if (error) {
+              console.log("--> error: " + error);
+            }
+
+            var heartRateCharacteristic = characteristics.find(function(el) {
+              return el.uuid = HR_CHARACTERISTIC_HEARTRATE_UUID;
+            });
+
+            heartRateCharacteristic.subscribe(function(error) {
+              if (error) {
+                console.log("--> subscribe error: " + error);
+              }
+            });
+
+            heartRateCharacteristic.on("data", function(data, isNotification) {
+              console.log(data.readUInt8(1));
+            });
+          });
         }
       });
     });
   }
 });
-
-
--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 ## How to use
 
 1. Run `node 1_scan_uuid.js` to get UUID of your sensor.
-2. Run `TICKR_UUID=uuid node 2_read_data.js` to get service for reading data.
+2. Run `TICKR_UUID=uuid node 2_read_data.js` to read heart rate values.
